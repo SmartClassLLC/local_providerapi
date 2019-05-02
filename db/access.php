576cca2972ffe738cff28f236534cc942e526396
caglar <ceremy@gmail.com>
2019-05-03 00:19:43 +0300

    first comment

---
+++ db/access.php
@@ -0,0 +1,99 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin capabilities are defined here.
+ *
+ * @package     local_providerapi
+ * @category    access
+ * @copyright   2019 Çağlar MERSİNLİ <ceremy@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+
+    'local/providerapi:get_shared_courses' => [
+        'captype' => 'view',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:get_site_info' => [
+        'captype' => 'view',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:enrol_course' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:add_group_to_course' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:add_grouping_to_course' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:create_user' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:edit_user' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:delete_user' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:delete_course_group' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+
+    'local/providerapi:edit_course_group' => [
+        'captype' => 'write',
+        'contextlevel' => 10,
+        'archetypes' => [
+        ],
+    ],
+];
---
+++ lang/en/local_providerapi.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_providerapi
+ * @category    string
+ * @copyright   2019 Çağlar MERSİNLİ <ceremy@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'providerapi';
+$string['providerapi:add_group_to_course'] = 'Kurslara grup Ekleme';
+$string['providerapi:add_grouping_to_course'] = 'Kurslara grouping ekleme';
+$string['providerapi:create_user'] = 'Kullanıcı Oluşturma';
+$string['providerapi:delete_course_group'] = 'kurs gruplarını silme';
+$string['providerapi:delete_user'] = 'Kullanıcı Silme';
+$string['providerapi:edit_course_group'] = 'kurs gruplarını düzenleme';
+$string['providerapi:edit_user'] = 'Kullanıcı Düzenleme';
+$string['providerapi:enrol_course'] = 'kurslara enrol olma';
+$string['providerapi:get_shared_courses'] = 'Paylaşılan kursları alma';
+$string['providerapi:get_site_info'] = 'Site Bilgisini Alma';
