399fb4fde49be53e66965561ba2af1ffcec24367
ceremy <ceremy@gmail.com>
2019-05-16 13:34:21 +0300

    fix class name

--- modules/course/index.php
+++ modules/course/index.php
@@ -25,7 +25,7 @@
  */
 
 use local_providerapi\local\course\course;
-use local_providerapi\output\course\table_batches;
+use local_providerapi\local\institution\institution;
 use local_providerapi\output\course\table_sharedcourses;
 
 require('../../../../config.php');
@@ -73,7 +73,7 @@ if (!$table->is_downloading()) {
             get_string('sharedcourse', 'local_providerapi'));
 
 }
-$institution = \local_providerapi\local\institution\institution::get($institutionid);
+$institution = institution::get($institutionid);
 list($select, $from, $where, $params) = course::get_sql($institutionid);
 $table->set_sql($select, $from, $where, $params);
 $table->set_istitutionname($institution->name);
